613910bbe5875a0ea71a1a656993b47687e15b32
Anish U K <anishkarthikeyan08@gmail.com>
2025-01-22 14:29:16 +0530

    Delete WorldWise directory

--- WorldWise/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { ESLint } from "eslint";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
